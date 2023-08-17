7d981103b6fc0f62b04ef4dac3553012c24cb7d1
BTH-IT <hungbienthanh@gmail.com>
2023-08-17 10:10:45 +0700

    first commit

---
+++ QuanLyCuaHangBanSach/DAO/DataProvider.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace QuanLyCuaHangBanSach.DAO
+{
+    public class DataProvider
+    {
+        private string serverName = "localhost";
+        private string databaseName = "qlchs";
+        private string username = "root";
+        private string password = "hung";
+        private MySqlConnection connect = null;
+        private static DataProvider instance;
+
+
+
+        public static DataProvider Instance {
+            get {
+                if (instance == null)
+                {
+                    instance = new DataProvider();
+                }
+
+                return DataProvider.instance;
+            }
+            set { DataProvider.instance = value; }
+        }
+
+        public DataProvider() {
+            connect = new MySqlConnection("SERVER=" + this.serverName + ";" +
+                    "DATABASE=" + this.databaseName + ";" + "UID=" + this.username + ";" +
+                    "PASSWORD=" + this.password + ";");
+        }
+
+        public DataProvider(string serverName, string databaseName, string username, string password)
+        {
+            connect = new MySqlConnection("SERVER=" + serverName + ";" +
+                "DATABASE=" + databaseName + ";" + "UID=" + username + ";" +
+                "PASSWORD=" + password + ";");
+        }
+    }
+}
