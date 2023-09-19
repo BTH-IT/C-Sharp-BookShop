112c3c82593244c9db820fb33e57a9299d616de3
BTH-IT <hungbienthanh@gmail.com>
2023-09-19 10:07:13 +0700

    add bill report and fix customer dto

--- QuanLyCuaHangBanSach/Program.cs
+++ QuanLyCuaHangBanSach/Program.cs
@@ -17,7 +17,7 @@ namespace QuanLyCuaHangBanSach
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new CustomerBillPrintForm(1));
+            Application.Run(new VendorGUI());
         }
     }
 }
